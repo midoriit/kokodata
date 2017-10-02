b3cf1ddb0b6b9140e484ef5115a96d03a11926d7
Takashi Koike <midoriit>
2017-10-02 18:59:30 +0900

    add link to external resources

--- js/kokodata.js
+++ js/kokodata.js
@@ -146,12 +146,14 @@ $(function(){
         var content = '';
         var prop;
         var prefix;
-        var link = true;
+        var isDate;
+        var link;
         for(i=0 ; i<list.length ; i++) {
 
           prop = list[i].p.value.replace(/.*prop\/direct\//g, '');
           prefix = '';
           link = true;
+          isDate = false;
 
           switch( prop ) {
 
@@ -167,6 +169,15 @@ $(function(){
             case 'P214':      // VIAF識別子
               prefix = 'http://viaf.org/viaf/';
               break;
+            case 'P227':      // GND識別子
+              prefix = 'http://d-nb.info/gnd/';
+              break;
+            case 'P244':      // LCNAF識別子
+              prefix = 'http://id.loc.gov/authorities/';
+              break;
+            case 'P271':      // CiNii著者識別子
+              prefix = 'http://ci.nii.ac.jp/author/';
+              break;
             case 'P349':      // 国立国会図書館典拠ID
               prefix = 'http://id.ndl.go.jp/auth/ndlna/';
               break;
@@ -178,6 +189,9 @@ $(function(){
               break;
             case 'P455':     // Emporis
               prefix = 'https://www.emporis.com/buildings/';
+              break;
+            case 'P757':      // 世界遺産識別子
+              prefix = 'http://whc.unesco.org/en/list/';
               break;
             case 'P935':      // コモンズのギャラリー
               prefix = 'https://commons.wikimedia.org/wiki/';
@@ -191,8 +205,17 @@ $(function(){
             case 'P1305':     // Skyscraper Center ID
               prefix = 'http://www.skyscrapercenter.com/building.php?building_id=';
               break;
+            case 'P1417':     // Encyclopaedia Britannica Online ID
+              prefix = 'https://www.britannica.com/';
+              break;
             case 'P1566':     // GeoNames ID
               prefix = 'http://www.geonames.org/';
+              break;
+            case 'P1612':     // Commons Institution page
+              prefix = 'https://commons.wikimedia.org/wiki/Institution:';
+              break;
+            case 'P1669':     // CONA ID
+              prefix = 'http://vocab.getty.edu/cona/';
               break;
             case 'P1699':     // SkyscraperPage building id
               prefix = 'http://skyscraperpage.com/cities/?buildingID=';
@@ -200,11 +223,17 @@ $(function(){
             case 'P2002':     // Twitterのユーザー名
               prefix = 'https://twitter.com/';
               break;
+            case 'P2003':     // インスタグラムのユーザー名
+              prefix = 'https://www.instagram.com/';
+              break;
             case 'P2013':     // フェイスブックID
               prefix = 'https://www.facebook.com/';
               break;
             case 'P2270':     // Emporis building complex ID
               prefix = 'https://www.emporis.com/complex/';
+              break;
+            case 'P2427':     // GRID ID
+              prefix = 'https://www.grid.ac/institutes/';
               break;
             case 'P2762':     // Skyscraper Center building complex ID
               prefix = 'https://www.skyscrapercenter.com/complex/';
@@ -216,17 +245,31 @@ $(function(){
               prefix = 'http://www.houjin-bangou.nta.go.jp/henkorireki-johoto.html?selHouzinNo=';
               break;
 
+            // 日付
+            case 'P571':      // 創立日
+            case 'P580':      // 開始日
+            case 'P1619':     // 開設年月日
+              link = false;
+              isDate = true;
+              break;
+
             default:
               link = false;
           }
           if(link) {
             content +=
-              list[i].propLabel.value + '(' + prop + ') : ' + 
+              list[i].propLabel.value + '<font color="#666">(' + prop + '):</font> ' + 
               '<a href="' + prefix + list[i].oLabel.value + '" target="_blank">' + 
               list[i].oLabel.value + '</a><br/>';
+          } else if(isDate) {
+            theDay = new Date(list[i].oLabel.value); 
+            content += 
+              list[i].propLabel.value + '<font color="#666">(' + prop + '):</font> ' +
+              theDay.getFullYear() + '年' + (theDay.getMonth()+1) + '月' + 
+              theDay.getDate() + '日<br/>';
           } else {
             content += 
-              list[i].propLabel.value + '(' + prop + ') : ' +
+              list[i].propLabel.value + '<font color="#666">(' + prop + '):</font> ' +
               list[i].oLabel.value + '<br/>';
           }
         }
